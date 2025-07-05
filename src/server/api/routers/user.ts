89b3c9343a9dc16bcab892281fa27e90061db138
himynameismaggie <mghosie@wpi.edu>
2025-07-05 17:24:32 -0400

    added user router

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,4 +1,65 @@
 import { z } from "zod";
 import { eq } from "drizzle-orm";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { projects } from "@/server/db/schema";
+import { users, candidates } from "@/server/db/schema";
+
+export const userRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1).max(255), email: z.string(), image: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.insert(users).values({
+        name: input.name,
+        email: input.email,
+        image: input.image,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }),
+
+  getOne: publicProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.query.users.findFirst({
+        where: (users, { eq }) => eq(users.id, input.id),
+      });
+    }),
+
+  getUsers: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.query.users.findMany({});
+  }),
+
+  getCandidates: publicProcedure.query(async ({ ctx }) => {
+    // return ctx.db.query.candidates.findMany({});
+    return ctx.db
+      .select({
+        candidate: candidates,
+        user: users,
+      })
+      .from(candidates)
+      .innerJoin(users, eq(candidates.userId, users.id));
+  }),
+
+  updateOne: publicProcedure
+    .input(
+      z.object({ id: z.string().uuid(), name: z.string().min(1).max(255), email: z.string(), image: z.string() }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db
+        .update(users)
+        .set({ name: input.name, email: input.email, image: input.image })
+        .where(eq(users.id, input.id));
+    }),
+
+  deleteOne: publicProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.delete(users).where(eq(users.id, input.id));
+    }),
+
+  deleteAll: publicProcedure.mutation(async ({ ctx }) => {
+    // eslint-disable-next-line drizzle/enforce-delete-with-where
+    return ctx.db.delete(users);
+  }),
+});
+
+export default userRouter;
