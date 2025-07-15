018d79fa4cc0b132a0d8589a17e74dc940e1943d
Matt <47070087+Camo651@users.noreply.github.com>
2025-07-15 13:47:07 -0400

    Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,16 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+const PrettierConfig = {
+    plugins: ["prettier-plugin-tailwindcss"],
+    trailingComma: "es5",
+    bracketSpacing: true,
+    objectWrap: "preserve",
+    singleQuote: true,
+    semi: true,
+    tabWidth: 4,
+    tabs: false,
+    endOfLine: "lf",
+    singleAttributePerLine: true,
+    printWidth: 999999,
 };
+
+export default PrettierConfig;
