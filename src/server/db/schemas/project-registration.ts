8cb2071af91fcd17ba53037d2f47264c02b181c1
himynameismaggie <mghosie@wpi.edu>
2025-08-02 14:03:12 -0400

    project registration schema

--- src/server/db/schemas/project-registration.ts
+++ src/server/db/schemas/project-registration.ts
@@ -1,18 +1,21 @@
 import { createId } from '@paralleldrive/cuid2';
 import { relations, sql } from 'drizzle-orm';
+import { primaryKey } from 'drizzle-orm/sqlite-core';
 import { createTable } from '../schema-util';
 import { adminProfiles, candidateProfiles } from './profiles';
 import { projects } from './projects';
-import { primaryKey } from 'drizzle-orm/sqlite-core';
 
 export const projectRegistrationQuestions = createTable('project_registration_question', (d) => ({
-    id: d.text().$defaultFn(() => createId()).primaryKey(),
+    id: d
+        .text()
+        .$defaultFn(() => createId())
+        .primaryKey(),
     question: d.text({ length: 512 }).notNull(),
     type: d.text({ enum: ['text', 'select', 'multi-select'] }).default('text'),
-    options: d.text(), 
+    options: d.text(),
     required: d.integer({ mode: 'boolean' }).default(true),
     createdBy: d.text({ length: 255 }).references(() => adminProfiles.userId, { onDelete: 'set null' }),
-}))
+}));
 
 export const projectRegistrationQuestionsRelations = relations(projectRegistrationQuestions, ({ many }) => ({
     projects: many(projectsToRegistrationQuestions),
@@ -20,9 +23,18 @@ export const projectRegistrationQuestionsRelations = relations(projectRegistrati
 }));
 
 export const projectRegistrations = createTable('project_registration', (d) => ({
-    id: d.text().$defaultFn(() => createId()).primaryKey(),
-    projectId: d.text().notNull().references(() => projects.id, { onDelete: 'cascade' }),
-    candidateId: d.text().notNull().references(() => candidateProfiles.userId, { onDelete: 'cascade' }),
+    id: d
+        .text()
+        .$defaultFn(() => createId())
+        .primaryKey(),
+    projectId: d
+        .text()
+        .notNull()
+        .references(() => projects.id, { onDelete: 'cascade' }),
+    candidateId: d
+        .text()
+        .notNull()
+        .references(() => candidateProfiles.userId, { onDelete: 'cascade' }),
     submittedAt: d.integer({ mode: 'timestamp' }).default(sql`(unixepoch())`),
     status: d.text({ enum: ['pending', 'accepted', 'rejected'] }).default('pending'),
 }));
@@ -40,9 +52,18 @@ export const projectRegistrationRelations = relations(projectRegistrations, ({ o
 }));
 
 export const projectRegistrationAnswer = createTable('project_registration_answer', (d) => ({
-    id: d.text().$defaultFn(() => createId()).primaryKey(),
-    registrationId: d.text().notNull().references(() => projectRegistrations.id, { onDelete: 'cascade' }),
-    questionId: d.text().notNull().references(() => projectRegistrationQuestions.id, { onDelete: 'cascade' }),
+    id: d
+        .text()
+        .$defaultFn(() => createId())
+        .primaryKey(),
+    registrationId: d
+        .text()
+        .notNull()
+        .references(() => projectRegistrations.id, { onDelete: 'cascade' }),
+    questionId: d
+        .text()
+        .notNull()
+        .references(() => projectRegistrationQuestions.id, { onDelete: 'cascade' }),
     answer: d.text({ length: 2048 }).notNull(),
 }));
 
@@ -57,16 +78,18 @@ export const projectRegistrationAnswerRelations = relations(projectRegistrationA
     }),
 }));
 
-export const projectsToRegistrationQuestions = createTable('projects_to_registration_questions', (d) => ({
-    projectId: d
-        .text()
-        .notNull()
-        .references(() => projects.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    questionId: d
-        .text()
-        .notNull()
-        .references(() => projectRegistrationQuestions.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    order: d.integer().notNull().default(0), 
+export const projectsToRegistrationQuestions = createTable(
+    'projects_to_registration_questions',
+    (d) => ({
+        projectId: d
+            .text()
+            .notNull()
+            .references(() => projects.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+        questionId: d
+            .text()
+            .notNull()
+            .references(() => projectRegistrationQuestions.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+        order: d.integer().notNull().default(0),
     }),
     (t) => [primaryKey({ columns: [t.projectId, t.questionId] })]
 );
